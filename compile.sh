f67b4cf69a58f46a646e44d98b021f360b90f6e7
yatsukha <yatsukha.roman@gmail.com>
2021-11-25 13:41:16 +0000

    Adjust em++ compilation

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-em++ -O3 -Iinclude -I. src/yl/**.cpp -std=c++17 --bind -o wasm/main.js --embed-file .predef.yl
+em++ -O3 -Iinclude -I. -Iimmer src/yl/**.cpp -std=c++17 --bind -o wasm/main.js --embed-file .predef.yl
